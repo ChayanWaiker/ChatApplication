ce7e4cc2a0afafadc2415675b7c3ad55fa40e4c6
ChayanWaiker <chayanwaiker1007@gmail.com>
2026-01-16 17:04:42 +0530

    fixes

--- backend/src/lib/socket.js
+++ backend/src/lib/socket.js
@@ -5,32 +5,35 @@ import express from "express";
 const app = express();
 const server = http.createServer(app);
 
+// used to store online users
+const userSocketMap = {}; // { userId: socketId }
 
 const io = new Server(server, {
   cors: {
-    origin: process.env.FRONTENDURL,
-    methods: ["GET", "POST"]
+    origin: process.env.FRONTENDURL, // https://chat-application-nodejs.netlify.app
+    credentials: true,               // ✅ REQUIRED for cookies
+    methods: ["GET", "POST"],
   },
 });
+
 export function getReceiverSocketId(userId) {
   return userSocketMap[userId];
 }
 
-// used to store online users
-const userSocketMap = {}; // {userId: socketId}
-
 io.on("connection", (socket) => {
-  console.log("A user connected", socket.id);
+  console.log("Socket connected:", socket.id);
 
   const userId = socket.handshake.query.userId;
-  if (userId) userSocketMap[userId] = socket.id;
+  if (userId) {
+    userSocketMap[userId] = socket.id;
+  }
 
-  // io.emit() is used to send events to all the connected clients
   io.emit("getOnlineUsers", Object.keys(userSocketMap));
 
   socket.on("disconnect", () => {
-    console.log("A user disconnected", socket.id);
-    delete userSocketMap[userId];
+    console.log("Socket disconnected:", socket.id);
+    if (userId) delete userSocketMap[userId];
+
     io.emit("getOnlineUsers", Object.keys(userSocketMap));
   });
 });
--- frontend/src/lib/axios.js
+++ frontend/src/lib/axios.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 export const axiosInstance = axios.create({
-  baseURL: `${import.meta.env.VITE_BACKENDURL}/api`,
+  baseURL:import.meta.env.MODE==='development'? `${import.meta.env.VITE_BACKENDURL}/api`:'/api',
   withCredentials: true,
 });
 
